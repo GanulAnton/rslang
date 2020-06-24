afc727e48c0e4fa25bcd169bb35d4e8794858d51
nofishtou <damnedbyhimself@gmail.com>
2020-06-25 00:29:26 +0300

    feat: add settings page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import obj from './main'
 
-import './styles.css'
-import image from  './assets/image.jpg'
+// import './styles.css'
+// import image from  './assets/image.jpg'
+
+import { Settings } from "./pages/settings/settings"
+
+const settings = Settings()
+const app = document.querySelector("#app")
+settings.onInit(app)
 
 console.log(obj);
-console.log('image', image);
+// console.log('image', image);
