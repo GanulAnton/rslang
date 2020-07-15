afc36df9e7a43730ab75c9f8a12ea0461b143055
Egor Badmaev <badmaeve2511@gmail.com>
2020-07-15 23:03:29 +0300

    feat: hangman

--- src/pages/wordBubbles/wordBubbles.js
+++ src/pages/wordBubbles/wordBubbles.js
@@ -5,6 +5,8 @@ export default function WordBubbles() {
   let state = {
     level: 0,
     isError: null,
+    firstLetters: null,
+    userWord: null
   }
 
   const onInit = (anchor) => {
@@ -19,274 +21,113 @@ export default function WordBubbles() {
     })
   }
 
-  function appendTimeout() {
-    const bodyTimer = containerRef.querySelector('#own-game-timer')
-    const output = document.createElement('span')
-    const tickingDown = containerRef.querySelector('#owngame-audio-tiking-down')
-    output.id = 'own-game-output'
-    const slider = document.createElement('div')
-    slider.id = 'own-game-slider'
-    bodyTimer.append(output)
-    bodyTimer.append(slider)
-
-    let time = 60,
-      fps = 60
-
-    let Timer = function(obj) {
-      this.time = obj.time
-      this.fps = obj.fps
-      this.onEnd = obj.onEnd || null
-      this.onStart = obj.onStart || null
-      this.onTick = obj.onTick || null
-
-      this.start = () => {
-        this.interval = setInterval(this.update, 1000 / this.fps)
-        this.onStart ? this.onStart() : void 0
-        return this
-      }
-      this.stop = () => {
-        clearInterval(this.interval)
-        this.onEnd ? this.onEnd() : void 0
-      }
-      this.update = () => {
-        this.time > 0 ? this.time -= 1 / this.fps : this.stop()
-        this.onTick ? this.onTick() : void 0
-        return this.get()
-      }
-      this.get = (par) => {
-        switch (par) {
-          case undefined:
-            return this.time
-          case 'dig':
-            return Math.ceil(this.time)
-          case 'end':
-            return this.onEnd()
-        }
-      }
-    }
-
-    let timer1 = new Timer({
-      time: time,
-      fps: fps,
-      onTick: tick,
-      //onEnd: endTimer,
-      onStart: onTimerStart
-    })
-
-    function onTimerStart() {
-      tickingDown.play()
-    }
-
-    function endTimer() {
-      gameResults()
-    }
-
-    timer1.start()
-    requestAnimationFrame(tick)
-
-    function tick() {
-      output.innerHTML = timer1.get('dig')
-      slider.style.width = timer1.get() / time * 100 + '%'
-    }
-  }
-
   const mainGame = () => {
-    let scorePoints = 0
-    let allEnteredWords = []
-    let res = []
-    state = {
-      level: containerRef.querySelector('.own-game-hardness').value - 1
-    }
-
     containerRef.innerHTML = `
-        <div class="own-game-main-container">
-          <div class="own-game-game-wrapper">
-            <div id="own-game-timer"></div>
-            <div id="own-game-score">
-              <h4 id="own-game-score-wrapper"></h4>
-            </div>
-            <form class="own-game-input-place">
-              <h4 class="own-game-first-letters"></h4>
-              <input class="own-game-game-input" type="text" autofocus spellcheck="false">
-              <button class="own-game-game-enter">Enter</button>
-            </form>
-            <audio id="owngame-audio-tiking-down" src="https://res.cloudinary.com/meta-modern/video/upload/v1593935234/55_smy6vv.mp3"></audio>
-            <audio id="owngame-audio-right-answer" src="https://res.cloudinary.com/meta-modern/video/upload/v1593953407/rightAnswerBubble_du1wub.mp3"></audio>
-            <audio id="owngame-audio-wrong-answer" src="https://res.cloudinary.com/meta-modern/video/upload/v1593953406/wrongAnswerBubble_zmnjrn.mp3"></audio>
-          </div>
-        </div>
+    <div class="own-game-container">
+      <div class="own-game-wrong-guesses">Wrong Guesses: <span id='mistakes'>0</span> of <span id='maxWrong'></span></div>
+        <div class="text-center">
+        <img id='hangmanPic' src="./assets/img/hangman/0.jpg" alt="hangman">
+        <p>Guess the Programming Language:</p>
+        <p id="wordSpotlight">The word to be guessed goes here</p>
+        <div id="keyboard"></div>
+      </div>
+    </div>
     `
-    const container = containerRef.querySelector('.own-game-main-container')
-    const input = containerRef.querySelector('input')
-    const gameInput = containerRef.querySelector('.own-game-game-input')
-    const form = containerRef.querySelector('.own-game-input-place')
-    const audioRightAnswer = containerRef.querySelector('#owngame-audio-right-answer')
-    const audioWrongAnswer = containerRef.querySelector('#owngame-audio-wrong-answer')
-    const gameScore = containerRef.querySelector('#own-game-score-wrapper')
-    const addGameScore = containerRef.querySelector('#own-game-score')
-    const gameScorePoints = document.createElement('p')
-    const firstLettersInRender = containerRef.querySelector('.own-game-first-letters')
-    const gameEnter = containerRef.querySelector('.own-game-game-enter')
     
-    gameScorePoints.classList.add('own-game-score-points')
-    container.style.backgroundImage = 'url(\'../../assets/img/wordbubbles/ownGame-gameBackground-3.png\')'
-    input.addEventListener('keyup', function() {
-      this.value = this.value.replace(/[^A-Za-z]/g, '')
-    })
-
-    const getWords = async () => {
-      const url = `https://afternoon-falls-25894.herokuapp.com/words?page=${Math.floor(Math.random() * 29)}&group=${state.level}`
-      const res = await fetch(url)
-      const json = await res.json()
-      let randomIndex = Math.floor(Math.random() * 19)
-      // firstLettersInRender.innerText = `${json[randomIndex].word.substring(0,2)}`
-      state = {
-        firstLetters: `${json[randomIndex].word.substring(0, 2)}`
-      }
-    }
-
-    const firstLetters = [
-      're', 'po', 'fa', 'bo', 'st', 'mo', 'su', 'of', 'as', 'co', 'di', 'in',
-      'un', 'ma', 'no', 'ob', 'po', 'qu', 'ra', 'ri', 'sa', 'sc', 'su', 'ta'
+    const programming_languages = [
+      "python",
+      "javascript",
+      "mongodb",
+      "json",
+      "java",
+      "html",
+      "css",
+      "c",
+      "csharp",
+      "golang",
+      "kotlin",
+      "php",
+      "sql",
+      "ruby"
     ]
-    //const firstLettersInRender = document.querySelector('.first-letters')
-    let firstLettersGlobal = `${firstLetters[Math.floor(Math.random() * 12)]}`
-    firstLettersInRender.innerText = firstLettersGlobal
-
-    form.addEventListener('submit', (e, test1) => {
-      e.preventDefault()
-
-      if (e.submitter.classList.contains('own-game-game-enter')) {
-        let checkMeWord = firstLettersGlobal + input.value
-        let userWord = checkMeWord.replace(/ +/g, '').trim().toLowerCase()
-        console.log('UserWord 153', userWord)
-        state = {
-          userWordToCheck: userWord,
-        }
-        yaSpeller().then(() => {
-        })
-        return userWord
+    
+    let answer = '';
+    let maxWrong = 6;
+    let mistakes = 0;
+    let guessed = [];
+    let wordStatus = null;
+    
+    function randomWord() {
+      answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
+    }
+    
+    function generateButtons() {
+      let buttonsHTML = 'qwertyuiopasdfghjklzxcvbnm'.split('').map(letter =>
+        `
+          <button
+            class="own-game-btn btn-lg btn-primary m-2"
+            id='` + letter + `'
+            value="` + letter +`"
+          >
+            ` + letter + `
+          </button>
+        `).join('');
+    
+      document.getElementById('keyboard').innerHTML = buttonsHTML;
+    }
+    
+    function handleGuess(chosenLetter) {
+      guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+      document.getElementById(chosenLetter).setAttribute('disabled', true);
+    
+      if (answer.indexOf(chosenLetter) >= 0) {
+        guessedWord();
+        checkIfGameWon();
+      } else if (answer.indexOf(chosenLetter) === -1) {
+        mistakes++;
+        updateMistakes();
+        checkIfGameLost();
+        updateHangmanPicture();
       }
-    })
-    const yaSpeller = async () => {
-      const url = `https://speller.yandex.net/services/spellservice.json/checkText?options=7&text=${state.userWordToCheck}`;
-      console.log('UserWord 166', state.userWordToCheck)
-      const res = await fetch(url);
-      const json = await res.json();
-      const data = JSON.stringify(json, null, 1)
-      state = {
-        isError: data
-      }
-      console.log(state.isError)
-    };
-
-
-      
-  function consoleLogFunc() {
-      console.log(state.isError)
-      if (state.isError.length >= 3) {
-        /* wordIsNotCorrect() */
-        console.log('Слово неправильно')
-      } else {
-        /* allEnteredWords.push(userWord)
-        wordIsCorrect() */
-        console.log('Слово правильно')
-      }
-  }
-
-  function wordIsCorrect() {
-        for (let i = 0; i < allEnteredWords.length; i++) {
-          if (!res.includes(allEnteredWords[i])) {
-            res.push(allEnteredWords[i])
-            audioRightAnswer.play()
-            gameInput.value = ''
-            if (allEnteredWords[i].length <= 4) {
-              scorePoints += 20
-              infoScorePoints(20)
-            } else if (allEnteredWords[i].length === 5 || allEnteredWords[i].length === 6) {
-              scorePoints += 40
-              infoScorePoints(40)
-            } else if (allEnteredWords[i].length === 7 || allEnteredWords[i].length === 8) {
-              scorePoints += 60
-              infoScorePoints(60)
-            } else if (allEnteredWords[i].length === 9 || allEnteredWords[i].length === 10) {
-              scorePoints += 80
-              infoScorePoints(80)
-            } else {
-              scorePoints += 100
-              infoScorePoints(100)
-            }
-            gameScore.innerHTML = `${scorePoints}`
-          }
-        }
-  }
-
-  function wordIsNotCorrect() {
-        audioWrongAnswer.play()
     }
 
-  function infoScorePoints(number) {
-        gameScorePoints.innerText = `+${number}`
-        addGameScore.append(gameScorePoints)
-  }
-  getWords().then(() => {
-      if (state.error) {
-        console.log("Произошла ошибка, связанная с Backend'ом таска RS Lang.")
-      }
-  })
-
-
-
-
-  }
-
-  function gameResults() {
-    containerRef.innerHTML = `
-        <div class="own-game-ending-container">
-          <div class="own-game-modal-window-ending">
-            <h1 id="own-game__result-h1">Results</h1>
-            <h2 id="own-game__result-is"></h2>
-            <section id="own-game__previous-results">
-              <h3 id="own-game__result-tryings"></h3>
-              <h3 id="own-game__result-maxscore"></h3>
-            </section>
-            <audio id="owngame-audio-game-ending" src="https://res.cloudinary.com/meta-modern/video/upload/v1593942147/kessidi-dzyn_mp3cut.net_1_1_a3bme0.mp3"></audio>
-          </div>
-        </div>
-    `
-
-    const resultIs = containerRef.querySelector('#own-game__result-is')
-    const bodyTimer = containerRef.querySelector('.own-game-modal-window-ending')
-    const gameEndingAudio = containerRef.querySelector('#owngame-audio-game-ending')
-    const amountOfTryings = containerRef.querySelector('#own-game__result-tryings')
-    const maxScoreRender = containerRef.querySelector('#own-game__result-maxscore')
-    const output = document.createElement('span')
-    const slider = document.createElement('div')
-
-    output.id = 'own-game-output'
-    slider.id = 'own-game-slider'
-
-    bodyTimer.append(output)
-    bodyTimer.append(slider)
-    gameEndingAudio.play()
-
-    resultIs.innerHTML = `Ваш результат: ${scorePoints} баллов`
-    output.style.display = 'none'
-    slider.style.display = 'none'
-
-    /* let amount = localStorage.getItem('Word Bubbles Score')
-
-    if (scorePoints > amount) {
-      localStorage.setItem('Word Bubbles Score', scorePoints)
-      maxScoreRender.innerHTML = `Наилучший результат: ${scorePoints}`
-    } else {
-      maxScoreRender.innerHTML = `Наилучший результат: ${amount}`
+    function updateHangmanPicture() {
+      document.getElementById('hangmanPic').src = './assets/img/hangman/' + mistakes + '.jpg';
     }
+    
+    function checkIfGameWon() {
+      if (wordStatus === answer) {
+        document.getElementById('keyboard').innerHTML = 'You Won!!!';
+      }
+    }
+    
+    function checkIfGameLost() {
+      if (mistakes === maxWrong) {
+        document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
+        document.getElementById('keyboard').innerHTML = 'You Lost!!!';
+      }
+    }
+    
+    function guessedWord() {
+      wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+    
+      document.getElementById('wordSpotlight').innerHTML = wordStatus;
+    }
+    
+    function updateMistakes() {
+      document.getElementById('mistakes').innerHTML = mistakes;
+    }
+    
+    document.getElementById('maxWrong').innerHTML = maxWrong;
+    
+    randomWord();
+    generateButtons();
+    guessedWord();
+    const buttonItems = document.querySelectorAll('.own-game-btn');
 
-    let amountOfTrying = localStorage.getItem('Word Bubbles Tryings')
-    amountOfTrying++
-    localStorage.setItem('Word Bubbles Tryings', amountOfTrying)
-    amountOfTryings.innerHTML = `Количество попыток: ${amountOfTrying}` */
+    buttonItems.forEach((buttonItem) => buttonItem.addEventListener('click', () => {
+      handleGuess(buttonItem.value)
+    }))
   }
 
   const render = () => {
